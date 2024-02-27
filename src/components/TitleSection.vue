cf94a9debd5da6483b3cd4187c6087e3695c1f92
Doonguin <penguinicesp@gmail.com>
2024-02-27 14:17:54 +0100

    intro of title section edited

--- src/components/TitleSection.vue
+++ src/components/TitleSection.vue
@@ -9,9 +9,7 @@
                 <br>
                 <b>Definition:</b> 
                 <br>
-                The process of embedding subtitles directly into video files, enhancing the viewing experience by providing text-based dialogue or captions synchronized with the video content.
-                <br><br>
-                Hey, welcome to the Subtitle Hardcoder Website! We're all about making movie nights a blast. Whether you're binge-watching classics or exploring foreign films, we've got your back. And guess what? It's all free, forever! So grab your popcorn, hit play, and let's take your movie nights to the next level!
+                Hardcoding, a masterful maneuver in video editing, involves covertly embedding subtitles into video content, akin to hiding Easter eggs in a blockbuster film. Like ninjas in the shadows, these subtitles stealthily convey dialogue, captions, and commentary, enriching your movie nights with laughter, emotion, and enlightenment.
             </p>
         </article>
     </section>
