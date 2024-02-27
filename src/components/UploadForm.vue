2a6d1dd0cf4287a9fdca72ed09509e221935077e
Doonguin <penguinicesp@gmail.com>
2024-02-27 12:19:21 +0100

    basic page + form setup + styling

---
+++ src/components/UploadForm.vue
@@ -0,0 +1,39 @@
+<template>
+    <section id="formSection">
+        <form>
+            <label for="videoInp" class="fileInput">
+                <span class="browseButton">Browse video files</span>
+                <input id="videoInp" type="file" accept=".mkv,.mp4" @change="getFileName($event, 'videoFile')">
+                <span id="videoName">{{ videoFile}}</span>
+            </label>
+
+            <label for="subsInp" class="fileInput">
+                <span class="browseButton">Browse sub files</span>
+                <input id="subsInp" type="file" accept=".vtt" @change="getFileName($event, 'subsFile')">
+                <span id="subsName">{{ subsFile }}</span>
+            </label>
+
+            <input type="submit" value="Start hardcoding">
+        </form>
+    </section>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            videoFile: 'No video file selected',
+            subsFile: 'No subtitle file selected'
+        };
+    },
+    methods: {
+        getFileName(event, fileNameType) {
+            const inp = event.target;
+            const fileName = inp.files.length > 0 ? inp.files[0].name : `No ${fileNameType === 'videoFileName' ? 'video' : 'subtitle'} file selected`;
+            this[fileNameType] = fileName;
+        }
+    }
+}
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
+// Script/Component imports
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// CSS imports
+import "./assets/css/variables.css"
+import "./assets/css/styles.css"
+import "./assets/css/UploadForm.css"
+
+// Vue app initialization
 createApp(App).mount('#app')
