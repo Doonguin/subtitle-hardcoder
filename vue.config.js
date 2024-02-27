2437bb4e2557cec8c596ffd003a5e4367d2780ae
Doonguin <penguinicesp@gmail.com>
2024-02-27 14:29:40 +0100

    build location changed

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ const path = require('path');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: Path2D.resolve(__dirname, "./server/public"),
+  outputDir: path.resolve(__dirname, "./server/public"),
 })
