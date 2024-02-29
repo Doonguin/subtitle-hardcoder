27e8c76e432bd59016dda3ee7852404b2b6bc869
Doonguin <penguinicesp@gmail.com>
2024-02-29 09:37:42 +0100

    changed font + file upload to server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,30 @@
 const multer = require('multer');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3081;
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename : (req, file, cb) => {
-        cb(null, file.originalname);
-    }
-});
+const dir = "uploads";
+let storage;
 
-const upload = multer({ storage: storage });
+const createSeedDir = () => {
+    const seed = Math.random().toString().slice(2, 9);
+
+    const destinationPath = `uploads/${seed}`;
+    fs.mkdirSync(destinationPath, { recursive: true });
+    fs.mkdirSync(`${destinationPath}/frames`, { recursive: true });
+
+    return storage = multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, destinationPath);
+        },
+        filename : (req, file, cb) => {
+            cb(null, file.originalname);
+        }
+    });
+}
+
+const upload = multer({ storage: createSeedDir() });
 
 app.post('/upload', upload.fields([
     { name: 'vid', maxCount: 1 },
@@ -32,4 +44,4 @@ app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
-})
+});
